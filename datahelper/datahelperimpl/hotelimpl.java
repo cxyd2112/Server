2d20a8def76c71d53921bb6f3e0470b5fb374856
huihantao <835964880@qq.com>
2016-12-17 20:37:28 +0800

    hht

---
+++ datahelper/datahelperimpl/hotelimpl.java
@@ -0,0 +1,7 @@
+package datahelper.datahelperimpl;
+
+/**
+ * Created by huihantao on 2016/12/7.
+ */
+public class hotelimpl {
+}
---
+++ datahelper/hotel.java
@@ -0,0 +1,12 @@
+package datahelper;
+
+/**
+ * Created by huihantao on 2016/12/7.
+ */
+public interface hotel {
+    String find(int hotelid);
+
+    boolean insert(String values);
+
+    boolean update(String values);
+}
